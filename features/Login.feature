c7b1a31e97666107e74a0f3993c6a262b77956aa
unknown <ekanaradhipad@gmail.com>
2023-05-04 16:08:38 +0700

    add go to login page

--- features/Login.feature
+++ features/Login.feature
@@ -2,6 +2,15 @@
 Feature: Login
 
 @Login
-Scenario: Login
+Scenario: Login Page
 Given I open voila
-And I fill email
+And I open Login Page
+Then I see Login Page
+
+@Login
+Scenario: Login Page
+When I see Login Page
+Then I enter email
+And I enter password
+And I submit
+Then I login
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -18,3 +18,39 @@ And('I fill valid email') do
 	#driver.find_element(:xpath, "//*[@class='Header_header__logo__JzRDc']").click
 	
 end
+
+Given('I open Login Page') do
+ # Write code here that turns the phrase above into concrete actions
+ driver.find_element(:xpath, "//*[@data-tooltip='Akun Saya']").click
+ 
+ driver.find_element(:xpath, "//*[@href='/account/login']").click
+
+end
+
+Then('I see Login Page') do
+  #assert_not_nil(driver.find_element(:xpath, "//*[@class='tt-title']"), "No response from Server!")
+  driver.find_element(:xpath, "//*[@class='tt-title']")
+  sleep(0.5) #wait for page to load
+end
+
+
+When('I enter email') do
+  # Write code here that turns the phrase above into concrete actions
+  driver.find_element(:xpath, "//*[@type='email']").click
+  driver.find_element(:xpath, "//*[@type='email']").send_key("ekanaradhipad@gmail.com")
+end
+
+When('I enter password') do
+  driver.find_element(:xpath, "//*[@type='password']").click
+  driver.find_element(:xpath, "//*[@type='password']").send_key("Password123")
+end
+
+Then('I submit') do
+  #element = driver.find_element(:xpath, "//*[@type='submit']")
+   driver.find_element(:xpath, "//*[@type='password']").click
+  #element_near.location_once_scrolled_into_view
+end
+
+Then('I login') do
+  driver.find_element(:xpath, "//*[@class='tt-title']")
+end
